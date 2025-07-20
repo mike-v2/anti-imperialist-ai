64e658b34b306c0699729900def1c20c97db0626
Mike McGuiness <mikemcguiness@protonmail.com>
2025-07-20 13:18:01 -0700

    Update app name at top of page

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -14,7 +14,7 @@ export function TopBar({ onMenuClick }: { onMenuClick: () => void }) {
       >
         <Menu className="h-6 w-6" />
       </Button>
-      <h1 className="text-xl font-semibold">Oracle</h1>
+      <h1 className="text-xl font-semibold">Anti-Imperialist AI</h1>
     </div>
   );
 } 
